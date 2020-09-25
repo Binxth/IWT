51e562e33f965e3ffb120d53a4026e00d3aa93cb
Binath Lakvidu <blakvidu@gmail.com>
2020-09-25 23:44:01 +0530

    Updated footer 25/09

--- script/javascript.js
+++ script/javascript.js
@@ -1,7 +1,47 @@
-var fruits, text, fLen, i; 
-fruits = ["Banana", "Orange", "Apple", "Mango"]; 
-fLen = fruits.length; text = "<ul>"; 
-for (i = 0; i < fLen; i++) 
-{ text += "<li>" + fruits[i] + "</li>"; } 
-text += "</ul>"; 
-document.getElementById("demo").innerHTML = text;
+var img = ["images/homeimg1.jpg", "images/homeimg2.png", "images/homeimg3.jpg"];
+		var x = 0;
+		
+		function next(i) {
+			
+			if (i == 1 )	{
+				
+				if (x == 0 || x == 1)	{
+					x++;
+					document.getElementById("sliderImage").src = img[x];
+					
+					if (x == 1)	{
+					document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 1";
+						}
+						else {
+							document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 2";
+							}
+				}
+				else {
+					x = 0; 
+					document.getElementById("sliderImage").src = img[x];
+					document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 3 ";
+				}
+			}
+			
+			else	{
+				
+				if (x == 1 || x == 2)	{
+					x--;
+					document.getElementById("sliderImage").src = img[x];
+				
+					if (x == 0)	{
+					document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 4";
+						}
+						else {
+							document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 5";
+							}
+				}
+				
+				else {
+					x = 2; 
+					document.getElementById("sliderImage").src = img[x];
+					document.getElementById("sliderImageText").innerHTML = "Teacher Trainer 6";
+				}
+			}
+		}
+		
