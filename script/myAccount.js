3eccba2fb0e9612016dd28fca207888965d19b47
Chamath Wijethilake <71723558+Chamaaah@users.noreply.github.com>
2020-10-19 03:34:34 +0530

    Add files via upload

---
+++ script/myAccount.js
@@ -0,0 +1,13 @@
+
+
+function inboxMsg()
+{
+  var txt;
+  if (confirm("You have 03 unread messages.")) {
+    txt = "You read all the messages";
+  } else {
+    txt = "Reminder: You have 03 unread messages";
+  }
+  document.getElementById("txtNotification").innerHTML = txt;
+
+}
